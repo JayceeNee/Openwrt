943e4c2dcee2e3a257512dca2c928114c7a8c3d1
JayceeNee <118952014+JayceeNee@users.noreply.github.com>
2022-11-24 13:33:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git luci-app-scutclient https://github.com/scutclient/luci-app-scutclient' >>feeds.conf.default
+#echo 'src-git luci-app-scutclient https://github.com/scutclient/luci-app-scutclient' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
