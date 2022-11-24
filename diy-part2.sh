8fd71ca95bb43a54e77f430cc06c821100cc5ca0
JayceeNee <118952014+JayceeNee@users.noreply.github.com>
2022-11-24 13:28:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,7 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+git clone https://github.com/scutclient/scutclient.git package/scutclient
+
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
